97bea273e0f501adf1ae692bc843e42d3df53474
ZatokaV <zatokav@gmail.com>
2025-09-02 14:25:52 +0300

    Move files

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&3^fiw10-wt)vm24832ad)j1&m-kxdtnox^0!j8kz6x*+q7qt_'
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
